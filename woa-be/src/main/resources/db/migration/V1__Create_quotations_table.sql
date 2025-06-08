7ee2f2266e01e5421fc8df2e51807d1b6feffe95
Kamil <valeriusderossa@gmail.com>
2025-06-08 22:34:36 +0200

    working migration

--- woa-be/src/main/resources/db/migration/V1__Create_quotations_table.sql
+++ woa-be/src/main/resources/db/migration/V1__Create_quotations_table.sql
@@ -12,11 +12,3 @@ CREATE TABLE quotations (
 CREATE INDEX idx_quotations_author ON quotations(author);
 CREATE INDEX idx_quotations_date ON quotations(date);
 CREATE INDEX idx_quotations_created_at ON quotations(created_at);
-
--- Add some sample data
-INSERT INTO quotations (author, quotation, date) VALUES 
-('Albert Einstein', 'Imagination is more important than knowledge. For knowledge is limited, whereas imagination embraces the entire world, stimulating progress, giving birth to evolution.', '1929-10-26'),
-('Maya Angelou', 'I''ve learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.', '1969-04-04'),
-('Nelson Mandela', 'Education is the most powerful weapon which you can use to change the world.', '1990-02-11'),
-('Steve Jobs', 'Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.', '2005-06-12'),
-('Martin Luther King Jr.', 'Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that.', '1963-08-28');
