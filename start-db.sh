4215b7246427fbdc30093f0f627998e66c42fc0e
Kamil <valeriusderossa@gmail.com>
2025-06-11 22:40:12 +0200

    working details

--- start-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd woa-be
-docker-compose up -d postgres
-echo "PostgreSQL container starting..."
-sleep 5
-echo "Database should be available on localhost:6666"
--- woa-be/src/main/kotlin/org/woa/dto/ThinkSummaryDto.kt
+++
@@ -1,2 +0,0 @@
-// This file is not used - removed to match the pattern of other modules
-// ThinkSummaryDto was removed to keep the structure consistent with other modules
